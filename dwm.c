138b405f0c8aa24d8a040cc1a1cf6e3eb5a0ebc7
Quentin Rameau <quinq@fifth.space>
2021-07-12 23:44:16 +0200

    Add a configuration option for fullscreen locking
    Some people are annoyed to have this new behaviour forced for some
    application which use fake fullscreen.

--- dwm.c
+++ dwm.c
@@ -835,7 +835,7 @@ focusstack(const Arg *arg)
 {
 	Client *c = NULL, *i;
 
-	if (!selmon->sel || selmon->sel->isfullscreen)
+	if (!selmon->sel || (selmon->sel->isfullscreen && lockfullscreen))
 		return;
 	if (arg->i > 0) {
 		for (c = selmon->sel->next; c && !ISVISIBLE(c); c = c->next);
