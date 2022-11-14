6d5279ff8f7c290ef2704c7792d6f239c5316a26
Christian Chiarulli <chris.machine@pm.me>
2022-11-14 18:39:13 -0500

    update zsh config to use zap

---
+++ zsh/.config/zsh/exports.zsh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# HISTFILE="$XDG_DATA_HOME"/zsh/history
+HISTSIZE=1000000
+SAVEHIST=1000000
+export EDITOR="nvim"
+export TERMINAL="kitty"
+export BROWSER="brave"
+export PATH="$HOME/.local/bin":$PATH
+export MANPAGER='nvim +Man!'
+export MANWIDTH=999
+export PATH=$HOME/.cargo/bin:$PATH
+export PATH=$HOME/.local/share/go/bin:$PATH
+export GOPATH=$HOME/.local/share/go
+export PATH=$HOME/.fnm:$PATH
+export PATH="$HOME/.local/share/neovim/bin":$PATH
+export XDG_CURRENT_DESKTOP="Wayland"
+#export PATH="$PATH:./node_modules/.bin"
+eval "$(fnm env)"
+eval "$(zoxide init zsh)"
+# eval "`pip completion --zsh`"
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$("$HOME/.miniconda/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$HOME/.miniconda/etc/profile.d/conda.sh" ]; then
+        . "$HOME/.miniconda/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/.miniconda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
