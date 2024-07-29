f169e9a39ffa216f78785115f79dcb5f0f78d7a5
Christian Chiarulli <chris.machine@pm.me>
2024-07-29 07:06:43 -0400

    updates

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -15,6 +15,7 @@ export PATH=$HOME/.local/share/go/bin:$PATH
 export GOPATH=$HOME/.local/share/go
 export PATH=$HOME/.fnm:$PATH
 export PATH="$HOME/.local/share/neovim/bin":$PATH
+export PATH="$HOME/.local/share/bob/nvim-bin":$PATH
 export XDG_CURRENT_DESKTOP="Wayland"
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_ENV_HINTS=1
